f2eb2e9f8648cf0dc046b68e613f365f2509af7d
lehason <lehason1009@gmail.com>
2025-10-25 21:46:45 +0700

    Delete src/assets directory

--- src/assets/images/index.js
+++
@@ -1,8 +0,0 @@
-export const images = {
-  background1: require('./background1.png'),
-  background2: require('./background1.png'),
-  background3: require('./background1.png'),
-  background4: require('./background1.png'),
-  background5: require('./background1.png'),
-  emptyCart: require('./empty_cart.png'),
-};
