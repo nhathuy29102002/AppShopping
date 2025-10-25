d754340dd176b7415ef4112607f223b0d57827fa
HansLangsdorff <huykhanh2929@gmail.com>
2025-10-25 14:48:54 +0700

    update

---
+++ src/screens/comments/commentList/CommentList.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Alert, FlatList, StyleSheet, View } from 'react-native';
+
+import { useTranslation } from 'react-i18next';
+
+import { commentApi } from '~/apis';
+import { FixedBottom, Header, LoadingIndicator } from '~/components';
+import { useGetListCommentRealtime } from '~/hooks';
+import { showMessage } from '~/utils';
+import { CommentItem } from './components';
+import { useNavigation } from '@react-navigation/native';
+import { SCREENS } from '~/constants';
+
+export const CommentList = () => {
+  const { t } = useTranslation();
+  const data = useGetListCommentRealtime();
+  const navigation = useNavigation();
+
+  const handleDelete = async (id) => {
+    Alert.alert(t('delete'), t('deleteMessage'), [
+      {
+        text: t('cancel'),
+        onPress: () => {},
+        style: 'cancel',
+      },
+      {
+        text: t('ok'),
+        onPress: () => deleteComment(id),
+        style: 'destructive',
+      },
+    ]);
+  };
+
+  const deleteComment = async (id) => {
+    const response = await commentApi.delete(id);
+    showMessage(response.message);
+  };
+
+  const handleReply = async (id) => {
+    navigation.navigate(SCREENS.COMMENT_REPLY, {
+      id,
+    });
+  };
+
+  return (
+    <>
+      <Header title={t('screenNames.commentList')} />
+      <FlatList
+        style={styles.list}
+        data={data}
+        renderItem={({ item }) => (
+          <CommentItem
+            data={item}
+            onDelete={() => {
+              handleDelete(item.id);
+            }}
+            onReply={() => {
+              handleReply(item.id);
+            }}
+          />
+        )}
+        ItemSeparatorComponent={() => <View style={styles.divider} />}
+        ListEmptyComponent={!data && <LoadingIndicator />}
+        ListFooterComponent={<FixedBottom />}
+      />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  list: {
+    paddingTop: 12,
+  },
+  divider: {
+    marginBottom: 4,
+  },
+});
